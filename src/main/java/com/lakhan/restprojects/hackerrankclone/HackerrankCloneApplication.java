b2f8214ed6999fa5ba3a0ba3433d592b244a1c7c
Lakhan Hake <hakelakhan@yahoo.com>
2021-03-19 22:01:50 +0530

    Added initial commit

---
+++ src/main/java/com/lakhan/restprojects/hackerrankclone/HackerrankCloneApplication.java
@@ -0,0 +1,13 @@
+package com.lakhan.restprojects.hackerrankclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakhan/restprojects/hackerrankclone/HackerrankCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakhan.restprojects.hackerrankclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerrankCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
